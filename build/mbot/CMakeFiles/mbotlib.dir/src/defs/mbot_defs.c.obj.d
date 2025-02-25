77ee844dddd3a818ee1462a68bd4823d38131c63
Andy Qin <primewolfa@gmail.com>
2025-02-25 16:31:16 -0500

    Initial fork

--- build/mbot/CMakeFiles/mbotlib.dir/src/defs/mbot_defs.c.obj.d
+++
@@ -1,3 +0,0 @@
-mbot/CMakeFiles/mbotlib.dir/src/defs/mbot_defs.c.obj: \
- /home/prime/medrob/mbot/src/defs/mbot_defs.c \
- /home/prime/medrob/mbot/include/mbot/defs/mbot_pins.h
--- build/rc/CMakeFiles/rclib.dir/src/math/algebra_common.c.obj.d
+++
@@ -1,3 +0,0 @@
-rc/CMakeFiles/rclib.dir/src/math/algebra_common.c.obj: \
- /home/prime/medrob/rc/src/math/algebra_common.c \
- /home/prime/medrob/rc/src/math/algebra_common.h
